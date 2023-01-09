e0904a0e3ad5f439dcccd83964834fc475fa0576
k.takayanagi <k.takayanagi@serendipy.jp>
2023-01-10 00:23:09 +0900

    初期登録

---
+++ gorouter_persisted_bottomnavbar/README.md
@@ -0,0 +1,6 @@
+# gorouter_persisted_bottomnavbar
+
+A sample project to challenge on persisting bottom navigation bar using `go_router` package.
+
+![persisted_bar_sample](https://user-images.githubusercontent.com/44666053/178017871-939f9053-c9d0-48c3-809f-5810558394f8.gif)
+
---
+++ gorouter_persisted_bottomnavbar/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gorouter_persisted_bottomnavbar/app_router.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends ConsumerWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // 定義したGoRouterクラスを取得
+    final router = ref.watch(routerProvider);
+
+    // MatrialApp.routerで囲う
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false, // debug表示を消す
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      // GoRouterクラスからrouteInformationProvider,routerInformationnParser, routerDelegateを渡す
+      routeInformationProvider: router.routeInformationProvider,
+      routeInformationParser: router.routeInformationParser,
+      routerDelegate: router.routerDelegate,
+    );
+  }
+}
